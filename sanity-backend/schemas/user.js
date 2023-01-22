aa7186dbbcab656db44ea675c71e5501d794949e
Vasil Hadzi-Tosev <vasil@stech.com>
2023-01-22 16:49:50 +0100

    Discover component finished

---
+++ sanity-backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'nameName',
+      title: 'User Name',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
+}
